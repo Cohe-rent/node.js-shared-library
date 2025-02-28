16a1f671dc05f974f01568752ba9a9bb47a54113
Cohe-rent <akshram.pandiyan@coherent.in>
2025-02-28 15:08:01 +0530

    Update deployApplication.groovy

--- vars/deployApplication.groovy
+++ vars/deployApplication.groovy
@@ -1,8 +1,15 @@
 def call() {
     stage('Deploy (Start Server)') {
-        steps {
+        script {
             container('nodejs') {
-                sh 'if [ -f service.js ]; then nohup node service.js & else echo "service.js not found, skipping..."; fi'
+                sh '''
+                if [ -f service.js ]; then
+                    nohup node service.js > server.log 2>&1 &
+                    echo "Server started successfully."
+                else
+                    echo "service.js not found, skipping..."
+                fi
+                '''
             }
         }
     }
