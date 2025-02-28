1c14cf369819d58f8fa38ed2a0955b67c4e981d7
Cohe-rent <akshram.pandiyan@coherent.in>
2025-02-28 15:36:44 +0530

    Update installDependencies.groovy

--- vars/installDependencies.groovy
+++ vars/installDependencies.groovy
@@ -1,17 +1,4 @@
+// vars/installDependencies.groovy
 def call() {
-    stage('Install Dependencies') {
-        script {
-            container('nodejs') {
-                sh '''
-                ls -la  # Debug: Check if package.json exists
-                node -v
-                if [ -f package.json ]; then
-                    npm install
-                else
-                    echo "package.json not found, skipping npm install."
-                fi
-                '''
-            }
-        }
-    }
+    sh 'npm install'
 }
