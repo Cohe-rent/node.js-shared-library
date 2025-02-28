e35f98457bbdbccf1654862da1f987afe40aed98
Cohe-rent <akshram.pandiyan@coherent.in>
2025-02-28 15:37:00 +0530

    Update runTests.groovy

--- vars/runTests.groovy
+++ vars/runTests.groovy
@@ -1,15 +1,4 @@
+// vars/runTests.groovy
 def call() {
-    stage('Run Tests') {
-        script {
-            container('nodejs') {
-                sh '''
-                if [ -f package.json ]; then
-                    npm test || echo "No tests found, skipping..."
-                else
-                    echo "package.json not found, skipping tests."
-                fi
-                '''
-            }
-        }
-    }
+    sh 'npm test'
 }
