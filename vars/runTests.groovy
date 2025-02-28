c4ec9182b333be396fb0f913b1e26763a01ae5e2
Cohe-rent <akshram.pandiyan@coherent.in>
2025-02-28 12:56:11 +0530

    Create runTests.groovy

---
+++ vars/runTests.groovy
@@ -0,0 +1,9 @@
+def call() {
+    stage('Run Tests') {
+        steps {
+            container('nodejs') {
+                sh 'npm test || echo "No tests found, skipping..."'
+            }
+        }
+    }
+}
