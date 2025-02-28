2cdc5f2e501b00766ee2029deb8b9e9f4c866e4f
Cohe-rent <akshram.pandiyan@coherent.in>
2025-02-28 13:04:12 +0530

    Delete installDependencies.groovy

--- installDependencies.groovy
+++
@@ -1,9 +0,0 @@
-def call() {
-    stage('Install Dependencies') {
-        container('nodejs') {
-            sh 'ls -la'  // Debug: Check if package.json exists
-            sh 'node -v'
-            sh 'npm install'
-        }
-    }
-}
